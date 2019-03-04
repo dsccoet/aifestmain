ae0dbda3227f88741b23cf3a306c886b5425163e
nrj <neerajkv2000@gmail.com>
2019-03-04 15:29:59 +0530

    Update apply.php

--- apply.php
+++ apply.php
@@ -2,7 +2,7 @@
 <?php
 	if(isset($_POST['newsletter_submit'])){
     
-    $email1 = pickup('email_newsletter');
+    $email = pickup('email_newsletter');
     
     include_once('conn.php');
 
@@ -10,15 +10,15 @@
 
     mysqli_query($con, "INSERT INTO newsletter(email,date_tab)
       
-      VALUES('$email1','$date')") or die("<script>
+      VALUES('$email','$date')") or die("<script>
       alert('Unable to register. Please try again.');
       window.location.href='index.php';
       </script>");
 
-      if($email1 != "NIL"){
+      if($email != "NIL"){
         $header1="From: no-reply@iedccoet.org";
           $subject1="REGISTRATION SUCCESFULL - AI FEST 2.0";
-          $to1 = $email1;
+          $to1 = $email;
           $message1 = "Hi,\n\nYour registration was succesfully recorded.\nWe are excited to have you onboard this event.\nOur team will get in touch with you soon.\nSee you then.\n\nRegards,\nTeam AI FEST 2.0,\nIEDC COET.";
 
           mail($to1,$subject1,$message1,$header1);
@@ -30,7 +30,7 @@
     
 
     echo "<script>
-      alert('Registration is succesfully recorded.');
+      alert('Subscription is succesfull.');
       window.location.href='index.php';
       </script>";
 
