417b2ebc85acc2fecf123c13c25539683ea8c1cb
Neeraj0002 <neerajkv2000@gmail.com>
2019-03-07 15:54:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -97,7 +97,7 @@
 
    //for the regsiter button countdown
     // Set the date we're counting down to
-    var countDownDate = new Date("Mar 6, 2019 23:00:00").getTime();
+    var countDownDate = new Date("Mar 7, 2019 24:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function() {
