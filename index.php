3e5f2320367d30c7d8141a6ed0377c566a66fec6
nrj <neerajkv2000@gmail.com>
2019-03-16 21:40:03 +0530

    Update index.php

--- index.php
+++ index.php
@@ -260,6 +260,69 @@
                <!--End row-->
             </div>
             <!--End container-->
+
+                        <div class="container">
+               <div class="container">
+                 <br>
+                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                   <!-- Indicators -->
+                   <ol class="carousel-indicators">
+                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                     <li data-target="#myCarousel" data-slide-to="1"></li>
+                     <li data-target="#myCarousel" data-slide-to="2"></li>
+                     <li data-target="#myCarousel" data-slide-to="3"></li>
+                     <li data-target="#myCarousel" data-slide-to="4"></li>
+                     <li data-target="#myCarousel" data-slide-to="5"></li>
+                     <li data-target="#myCarousel" data-slide-to="6"></li>
+                   </ol>
+
+                   <!-- Wrapper for slides -->
+                   <div class="carousel-inner">
+
+                     <div class="item active">
+                       <img src="pst/001.png" alt="Los Angeles" style="width:100%;">
+                     </div>
+
+                     <div class="item">
+                       <img src="pst/002.png" alt="Chicago" style="width:100%;">
+                     </div>
+                   
+                     <div class="item">
+                       <img src="pst/003.png" alt="New York" style="width:100%;">
+                     </div>
+
+                     <div class="item">
+                       <img src="pst/004.png" alt="New York" style="width:100%;">
+                     </div>
+
+                     <div class="item">
+                       <img src="pst/005.png" alt="New York" style="width:100%;">
+                     </div>
+
+
+                     <div class="item">
+                       <img src="pst/007.png" alt="New York" style="width:100%;">
+                     </div>
+
+                     <div class="item">
+                       <img src="pst/008.png" alt="New York" style="width:100%;">
+                     </div>
+                 
+                   </div>
+
+                   <!-- Left and right controls -->
+                   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+                     <span class="glyphicon glyphicon-chevron-left"></span>
+                     <span class="sr-only">Previous</span>
+                   </a>
+                   <a class="right carousel-control" href="#myCarousel" data-slide="next">
+                     <span class="glyphicon glyphicon-chevron-right"></span>
+                     <span class="sr-only">Next</span>
+                   </a>
+                 </div>
+               </div>
+            </div>
+
          </section>
          <!--End about section-->
          <!--Speakers section-->
@@ -526,71 +589,7 @@
                </div>
                <!--end of row-->
             </div>
-            <div class="container">
-               <div class="container">
-                 <br>
-                 <div id="myCarousel" class="carousel slide" data-ride="carousel">
-                   <!-- Indicators -->
-                   <ol class="carousel-indicators">
-                     <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                     <li data-target="#myCarousel" data-slide-to="1"></li>
-                     <li data-target="#myCarousel" data-slide-to="2"></li>
-                     <li data-target="#myCarousel" data-slide-to="3"></li>
-                     <li data-target="#myCarousel" data-slide-to="4"></li>
-                     <li data-target="#myCarousel" data-slide-to="5"></li>
-                     <li data-target="#myCarousel" data-slide-to="6"></li>
-                     <li data-target="#myCarousel" data-slide-to="7"></li>
-                   </ol>
 
-                   <!-- Wrapper for slides -->
-                   <div class="carousel-inner">
-
-                     <div class="item active">
-                       <img src="pst/001.png" alt="Los Angeles" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/002.png" alt="Chicago" style="width:100%;">
-                     </div>
-                   
-                     <div class="item">
-                       <img src="pst/003.png" alt="New York" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/004.png" alt="New York" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/005.png" alt="New York" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/006.png" alt="New York" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/007.png" alt="New York" style="width:100%;">
-                     </div>
-
-                     <div class="item">
-                       <img src="pst/008.png" alt="New York" style="width:100%;">
-                     </div>
-                 
-                   </div>
-
-                   <!-- Left and right controls -->
-                   <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                     <span class="glyphicon glyphicon-chevron-left"></span>
-                     <span class="sr-only">Previous</span>
-                   </a>
-                   <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                     <span class="glyphicon glyphicon-chevron-right"></span>
-                     <span class="sr-only">Next</span>
-                   </a>
-                 </div>
-               </div>
-            </div>
          </section>
          <!--End topics section-->
          <!--Schedule section-->
